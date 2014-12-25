88444141a8aa334a6c4d7f0c1bea88efc121a8c4
Alex Heritier <alex.heritier@gmail.com>
2014-12-25 02:17:48 -0800

    minor comment changes + string.xml name changes

--- loginManagers/app/src/main/java/com/ci/generalclasses/loginmanagers/LoginManager.java
+++ loginManagers/app/src/main/java/com/ci/generalclasses/loginmanagers/LoginManager.java
@@ -27,50 +27,24 @@ public class LoginManager {
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static String TWITTER_KEY;
     private static String TWITTER_SECRET;
+
     /**
-     * Before using getFBLogin, you need to do the following
-     * 1. add the following to AndroidManifest.xml
-     *  - these go just under manifest
-     *      - <meta-data android:name="com.facebook.sdk.ApplicationId" android:value="@string/app_id"/>
-     *      - <uses-permission android:name="android.permission.INTERNET" />
-     *  - these go under application
-     *      - <uses-permission android:name="android.permission.SET_DEBUG_APP"/>
-     *      - <activity
-     android:name="com.facebook.LoginActivity"
-     android:label="@string/app_name" >
-     <intent-filter>
-     <action android:name="android.intent.action.MAIN" />
-     <category android:name="android.intent.category.LAUNCHER" />
-     </intent-filter>
-     </activity>
-     * 2. add the following to strings.xml
-     *  - <string name="app_id">YOUR APP ID HERE</string>
-     * 3. add the following to activity_my.xml
-     *  - <com.facebook.widget.LoginButton
-     android:id="@+id/fb_auth_buttonon"
-     android:layout_width="wrap_content"
-     android:layout_height="wrap_content"
-     android:layout_gravity="center_horizontal"
-     android:layout_marginTop="30dp"
-     />
-     *
-     *  - ^This adds the facebook login button, so you can change its styling however you like
-     * 4. add the following to your activity's onCreate method
-     *  - if (savedInstanceState == null) {
-             getSupportFragmentManager()
-             .beginTransaction()
-             .add(android.R.id.content, THE NAME OF YOUR FBFragment)
-             .commit();
-             } else {
-             // Or set the fragment from restored state info
-             // this will be custom code
-             }
-     *
+     * Before using this API: https://github.com/Combustion-Innovation/general-classes/wiki/Facebook-Login-Requirements
+     * API info: https://github.com/Combustion-Innovation/general-classes/wiki/Facebook-Login-API
+     * 
+     * @return
      */
     public static FBFragment getFBLogin() {
         return new FBFragment();
     }
 
+    /**
+     * https://github.com/Combustion-Innovation/general-classes/wiki/Proprietary-Login-requirements
+     * https://github.com/Combustion-Innovation/general-classes/wiki/Proprietary-Login-API
+     *
+     * @param activity
+     * @return
+     */
     public static ProprietaryFragment initProprietaryLogin(final Activity activity) {
         final ProprietaryFragment proprietaryFragment = new ProprietaryFragment();
         proprietaryFragment.setActivity(activity);
@@ -90,13 +64,20 @@ public class LoginManager {
         return proprietaryFragment;
     }
 
+    /**
+     * https://github.com/Combustion-Innovation/general-classes/wiki/Twitter-Login-requirements
+     * https://github.com/Combustion-Innovation/general-classes/wiki/Facebook-Login-API
+     *
+     * @param key consumer key
+     * @param secret secret key
+     */
     public static void setTwitterKeys(String key, String secret) {
         TWITTER_KEY = key;
         TWITTER_SECRET = secret;
     }
 
     public static TwitterLoginButton initTwitterLogin(final Activity activity, Callback<TwitterSession> callback) {
-        TwitterLoginButton twitterLoginButton = (TwitterLoginButton) activity.findViewById(R.id.twitter_login_button);;
+        TwitterLoginButton twitterLoginButton = (TwitterLoginButton) activity.findViewById(R.id.twitter_login_button);
         final TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
 
         Fabric.with(activity, new Twitter(authConfig));
