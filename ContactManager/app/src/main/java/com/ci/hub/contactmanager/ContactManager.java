3fcd1dbaa82e467f5ac9d35798c5cae10eb55620
Alex Heritier <alex.heritier@gmail.com>
2015-01-12 16:04:01 -0800

    Added a confirmation dialog to the ContactManager

--- ContactManager/app/src/main/java/com/ci/hub/contactmanager/ContactManager.java
+++ ContactManager/app/src/main/java/com/ci/hub/contactmanager/ContactManager.java
@@ -1,12 +1,21 @@
 package com.ci.hub.contactmanager;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Alex on 1/7/15.
@@ -30,36 +39,48 @@ import java.util.List;
 public class ContactManager {
     public static final String TAG = "ContactManager";
 
-    private Activity activity;
-    private boolean contactPermission;
+    private static Activity activity;
+    private static boolean contactPermission;
 
-    private List getAllContacts() {
-        String[] mProjection = new String[]
-                {
-                        ContactsContract.Profile._ID,
-                        ContactsContract.Profile.DISPLAY_NAME_PRIMARY,
-                        ContactsContract.Profile.LOOKUP_KEY,
-                        ContactsContract.Profile.PHOTO_THUMBNAIL_URI
-                };
+    private static List getAllContacts() {
+        List<HashMap<String, String>> contacts = new ArrayList<HashMap<String, String>>(2);
 
-        // Retrieves the profile from the Contacts Provider
-        Cursor mProfileCursor = activity.getContentResolver().query(
-                ContactsContract.Contacts.CONTENT_URI,
-                mProjection,
-                null,
-                null,
-                null);
+        ContentResolver cr = activity.getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
-        Log.d(TAG, mProfileCursor.getCount() + "");
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                HashMap<String, String> contact = new HashMap<String, String>();
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                contact.put("name", name);
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    System.out.println("name : " + name + ", ID : " + id);
 
-        return null;
+                    // get the phone number
+                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phone = pCur.getString(
+                                pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        System.out.println("phone" + phone);
+                        contact.put("phone", phone);
+                    }
+                    pCur.close();
+                }
+                contacts.add(contact);
+            }
+        }
+
+        return contacts;
     }
 
     public ContactManager(Activity activity) {
         this.activity = activity;
         contactPermission = false;
 
-        getAllContacts();
+        askForContactsPermission();
     }
 
     public boolean hasContactPermission() {
@@ -67,18 +88,58 @@ public class ContactManager {
     }
 
     public List<String> getContactsNumbers() {
-        List<String> numbers = new ArrayList<String>();
-        // collect numbers...
-        return numbers;
+        if (hasContactPermission()) {
+            // collect numbers...
+            List<HashMap<String, String>> contacts = getAllContacts();
+            List numbers = new ArrayList<String>();
+            for (int i = 0; i < contacts.size(); i++) {
+                numbers.add(contacts.get(i).get("phone"));
+            }
+            return numbers;
+        }
+        return null;
     }
 
     public List<String> getContactsNames() {
-        List<String> names = new ArrayList<String>();
-        // collect names
-        return names;
+        if (hasContactPermission()) {
+            // collect names
+            List<HashMap<String, String>> contacts = getAllContacts();
+            List names = new ArrayList<String>();
+            for (int i = 0; i < contacts.size(); i++) {
+                names.add(contacts.get(i).get("name"));
+            }
+            return names;
+        }
+        return null;
     }
 
-    public void startGettingContacts() {
+    public void askForContactsPermission() {
+        GetContactsConfirmationFragment confirmationDialog = new GetContactsConfirmationFragment();
+        confirmationDialog.show(activity.getFragmentManager(), "ContactsDialog");
+    }
 
+    public static class GetContactsConfirmationFragment extends DialogFragment {
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Allow this app to access your contacts?")
+                    .setTitle("Contacts")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Log.d(TAG, "We have permission!");
+                            contactPermission = true;
+                            getAllContacts();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Log.d(TAG, "We don't have permission!");
+                            contactPermission = false;
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
     }
 }
---
+++ ContactManager/app/src/main/java/com/ci/hub/contactmanager/GetContactsConfirmationFragment.java
@@ -0,0 +1,13 @@
+package com.ci.hub.contactmanager;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Alex on 1/12/15.
+ */
+
